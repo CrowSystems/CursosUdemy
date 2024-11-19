92331b7a4ad31813be84f4dd764e902d0d3bde50
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-11-19 17:37:01 -0600

    Inicio de curso de PHP

---
+++ PHP/PHPyMysqlPractico/01variables.php
@@ -0,0 +1,22 @@
+<?php
+
+#String: Cadena de texto
+$nombre = 'Carlos';
+
+#Integer: Numeros enteros
+$numero = 7;
+
+#Double: Numeros con decimales
+$numero_decimal = 7.7;
+
+#Boolean: Verdadero o Falso (true / false)
+
+/**
+ *  Tipos de variables en PHP
+ *  Array: Arreglo
+ *  Object: Objeto
+ *  Class: Clase
+ *  Null: Cuando a una variable aun no se le ha asignado ningun valor
+ */
+
+?>
