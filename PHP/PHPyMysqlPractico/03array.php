a15cfc7520fa3dabc942bb8e3c7530e16fc3142e
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-11-19 22:34:50 -0600

    clase de manejo de array con php

---
+++ PHP/PHPyMysqlPractico/03array.php
@@ -0,0 +1,8 @@
+<?php
+
+$semana = array('Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo');
+$arreglo = array('cadenas de texto', 1, array('asd', 123), true);
+
+echo $semana[0] . '<br />';
+echo $semana[2] . '<br />';
+?>
