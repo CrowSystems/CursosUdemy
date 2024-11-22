a30c79e2112bd23b79a44ee439443c22495a9780
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-11-22 15:26:52 -0600

    Clase: manejo de condicionales IF

---
+++ PHP/PhpyMysql_Basico/Introduccion/cicloif.php
@@ -0,0 +1,13 @@
+<?php
+/*
+El constructor if es una de las caracteristicas más importantes de muchos lenguajes, permite la ejecución condicional defragmentos de código
+la expresión es evaluada a su valor booleano, si la expresión se evaluúa como true, PHP ejecutará la sentencia y si se evalúa como false la 
+ignorá.
+ */
+$edad = 18;
+if($edad == 18){
+    echo "somos mayores de edad";
+} else {
+    echo "somos menores de edad";
+}
+?>
