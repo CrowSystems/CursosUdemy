b2af7c58ee377054243ec747f9f6f1ed6f43917f
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-11-22 14:03:10 -0600

    Clase de Constantes

---
+++ PHP/PhpyMysql_Basico/Introduccion/constantes.php
@@ -0,0 +1,10 @@
+<?php
+/* Un constante en PHP son identificadores de valores que no cambian durante la ejecución de un programa, son herramientas muy utiles 
+para garantizar la integridad de los datos en una aplicación y estas se utilizan para cuando tienes valores fijos que nunca van a cambiar
+dentro de tu programa.
+ */
+define('constante', 'hola');
+define('contante', 'buen día');
+
+echo constante;
+?>
