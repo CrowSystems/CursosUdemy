23c8cf128003ae6371e20340dc2106d25dd4dfb4
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-11-23 13:35:47 -0600

    carga de Ciclos switch case

---
+++ PHP/PhpyMysql_Basico/Introduccion/switchCase.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Clase de Sewitch case
+*/
+$opc = 2;
+switch($opc){
+    case 1:
+        echo "valor es 1";
+        break;
+    case 2:
+        echo "valor es 2";
+        break;
+    case 3:
+        echo "valor es 3";
+        break;
+}
+?>
