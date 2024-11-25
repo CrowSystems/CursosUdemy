b18760647e6eaa0f7062a106c4874361d59b91d6
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-11-24 22:37:24 -0600

    Clase de manejo decondicionales

---
+++ PHP/PhpyMysql_Basico/Introduccion/operadoresLogicos.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Tipos de condicionales:
+ * AND &&
+ * OR ||
+ * gmp_xor(a, b)
+ */
+$a = 1;
+$b = 2;
+if ($a == 1 AND $b == 2) {
+    echo "Cumple la condicion";
+} else {
+    echo "no cumple la condicion";
+} 
+?>
