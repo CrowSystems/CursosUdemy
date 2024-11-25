aa5539e3da022e9043d812fd4962a965f0b01d11
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-11-24 22:30:57 -0600

    Clase de manejo de fechas en PHP

---
+++ PHP/PhpyMysql_Basico/Introduccion/Fecha.php
@@ -0,0 +1,15 @@
+<?php
+echo "fecha del dia de hoy: ". date('d-m-Y');
+echo "<br>";
+echo "El día de hoy es: ".date('d');
+echo "<br>";
+echo "El mes actual es: ".date('M');
+echo "<br>";
+echo "El mes actual es: ".date('F');
+echo "<br>";
+echo "el número de semana actual: ".date('W');
+echo "<br>";
+echo "el numero del día miercoles es: ".date('w');
+echo "<br>";
+echo "cuantos dias tienen el mes: ".date('t');
+?>
--- PHP/PhpyMysql_Basico/Introduccion/while.php
+++ PHP/PhpyMysql_Basico/Introduccion/while.php
@@ -4,7 +4,6 @@ $i = 0;
 while($i <= 10) {
     echo $i;
     echo "<br>";
-    $i++;
+    $i++; 
 }
-  
 ?>
