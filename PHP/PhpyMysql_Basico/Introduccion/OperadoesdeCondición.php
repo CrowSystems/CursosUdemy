b22dab624817caae8ef909cae729c895c1da6255
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-11-26 00:25:58 -0600

    Clase de manejo de Operadores de comparación

---
+++ PHP/PhpyMysql_Basico/Introduccion/OperadoesdeCondición.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *  == Iagual
+ * === Identico
+ * != Diferente <>
+ * <= menor o igual qie
+ * >= mayor o igual que
+ * 
+ *  */
+
+ $var = 20;
+ if ($var >= 100) {
+    echo "Es mayor a 100";
+ } else if($var == 20) {
+    echo "es igual a 20";
+ }
+?>
