5030907a9f714a0408188c05c7bd814e052e367f
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-11-28 20:45:12 -0600

    Inicio de clases con PHP

---
+++ 01-ProgramacionWeb/MasterPHPSQLMVC/php/variables.php
@@ -0,0 +1,9 @@
+<?php
+//Una variable es un contenedor de información, puedo guardar cualquier dato dentro de ella
+$mi_primera_variable = "Hola mundo una variable";
+$numero = 44;
+$verdadero = true;
+
+echo '<h1>'.$mi_primera_variable.'</h1>';
+echo $numero."<br>";
+?>
