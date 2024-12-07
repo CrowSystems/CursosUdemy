bcd5b8dc8d7be1d9a5ad4389567bb9a6eccb24c6
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-12-07 07:46:38 -0600

    Clase de costantes

--- 01-ProgramacionWeb/MasterPHPSQLMVC/php/constantes.php
+++ 01-ProgramacionWeb/MasterPHPSQLMVC/php/constantes.php
@@ -1,3 +1,17 @@
 <?php
+//Constantes
+//Es un contenedor de información que no puede variar
+
+define('nombre', 'Carlos De jesus');
+define('web', 'crowsystems.com.mx');
+
+echo '<h1>'.nombre.'</h1>';
+echo '<h1>'.web.'</h1>';
+
+//Variable
+$web = "crowsystems.com.mx/academy";
+$web = "crowsystems.com.mx/cursos";
+echo '<h1>'.$web.'</h1>';
+
 
 ?>
