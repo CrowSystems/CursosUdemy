fa36546b9c8fb385325339fdaef4ee45e2f8d549
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-12-06 22:17:34 -0600

    Clase de constantes y tipos de variables

---
+++ 01-ProgramacionWeb/MasterPHPSQLMVC/php/tiposVariables.php
@@ -0,0 +1,19 @@
+<?php 
+/*
+Entero (int / integer) = 99
+
+*/
+$numerohola = 100;
+$decimal = 27.9;
+$texto = "soy un texto";
+$verdadero = false;
+$nula = null;
+
+echo gettype($nula);
+
+//debugear
+$mi_nombre[] = "Victor Robles WEB";
+$mi_nombre[] = "Victor Robles WEB";
+
+//var_dump[$mi_nombre]
+?>
