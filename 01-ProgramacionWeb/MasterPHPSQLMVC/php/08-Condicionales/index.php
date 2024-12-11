3082e516291acedc26556b71f587c0eb10d7d013
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-12-11 15:46:02 -0600

    Clase: Condicional IF

--- 01-ProgramacionWeb/MasterPHPSQLMVC/php/08-Condicionales/index.php
+++ 01-ProgramacionWeb/MasterPHPSQLMVC/php/08-Condicionales/index.php
@@ -19,7 +19,7 @@
  * <=  MENOR O IGUAL QUE
  * >=  MAYOR O IGUAL QUE
 */
-
+//Ejemplo 1
 $color = "rojo";
 
 if($color == "verde"){
@@ -30,10 +30,31 @@ if($color == "verde"){
 
 echo "<br>";
 
+//Ejemplo 2
 $year = 2028;
 if($year != 2019){
     echo "Es una año diferente a 2019";
 } else {
     echo "es un año posterior al 2019";
 }
+
+//Ejemplo 3
+$nombre = "David Extremadura";
+$edad = 18;
+$mayoriaEdad = 18;
+$continente ="Europa";
+$ciudad = "Madrid";
+
+if($edad >= $mayoriaEdad){
+    echo "<h1>$nombre es mayor de edad</h1>";
+
+    if($continente == "Europa"){
+        echo "<h2>Y es  de $ciudad</h2>";
+    } else {
+        echo "No es de Europa";
+    } 
+} else {
+    echo "<h2>$nombre No es de mayor de edad</h2>";
+}
+
 ?>
