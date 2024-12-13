8edd5132683b847b8825b2b3d746d7ea4ca7e2fd
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-12-12 19:57:18 -0600

    Ejemplo de bucles de tipo For

--- 01-ProgramacionWeb/MasterPHPSQLMVC/php/09-Bucles/bucleFor.php
+++ 01-ProgramacionWeb/MasterPHPSQLMVC/php/09-Bucles/bucleFor.php
@@ -12,5 +12,14 @@ for($i = 0; $i <= 100; $i++){
     $resultado += $i;
     echo "<p>$resultado</p>";
 }
+
 echo "<h1>El resultado es: $resultado </h1>";
+
+for($contador = 1; $contador <=10; $contador++){
+    if($numero == 45){
+        echo "No se pueden monstrar estas operaciones prohibidas";
+    }
+    echo "$numero x $contador = ".($numero * $contador)."<br/>";
+}
+
 ?>
