272dff0c1ac397a8d73f547e8683215617f611e8
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-12-17 16:20:44 -0600

    Inicio con trabajo con Mysql

---
+++ 01-ProgramacionWeb/MasterPHPSQLMVC/sql/09-Ejercicios/ejercicios.sql
@@ -0,0 +1,24 @@
+CREATE TABLE COCHES INSERT INTO (
+    id,
+    modelo,
+    marca,
+    precio,
+    stock
+);
+
+CREATE TABLE CLASE INSERT INTO(
+    id,
+    nombre,
+    ciduad
+);
+
+CREATE TABLE VENDEDOR INSERT INTO(
+    id,
+    nombre,
+    apellidos,
+    cargo,
+    fehcaAlta,
+    sueldo,
+    comision,
+    jefe
+);
