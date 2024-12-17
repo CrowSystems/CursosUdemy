7759abbc6fc9e65578c53272934bd417e185fb48
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2024-12-17 16:47:00 -0600

    Ejercicios 1 en PHP

---
+++ 01-ProgramacionWeb/MasterPHPSQLMVC/php/10-Ejercicios/ejercici1.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Ejercicio 1
+ * Crear dos variables "Pais" y "Continente" y mostrar su valor por pantalla(imprimir)
+ * poner en un comentario que tipo de dato tienen
+ * 
+ */
+$pais = "Mexico";
+$continente = "Norte America";
+
+//Tipo de dato que tiene el registro es String
+echo "El pais es: ".$pais."<br/>";
+echo "El contienente es: ".$continente."<br/>";
+?>
